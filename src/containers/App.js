cd83d7a5494a602ecdf262f68fc6faaa51390e2c
Fiveko <pavlovskipetko@abv.bg>
2019-05-02 19:02:02 +0300

    project finished

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,11 +4,14 @@ import CardList from "../ components/CardList";
 import SearchBox from "../ components/SearchBox";
 import Scroll from '../ components/Scroll';
 import "./App.css";
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchRobots.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
@@ -16,45 +19,33 @@ const mapDispatchToProps = (dispatch) => {
   return{
   onSearchChange: (event) => dispatch(setSearchField(
     event.target.value
-  ))
+  )),
+  onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: []
-    }
-  }
 
   componentDidMount() {
-    console.log(this.props.store.getState())
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then(res => {
-        return res.json();
-      }) 
-      .then(users => {
-        this.setState({ robots: users });
-      });
+    this.props.onRequestRobots();
   }
 
   render() {
-    const {robots} = this.state;
-    const {searchField, onSearchChange} = this.props;
+    const {searchField, onSearchChange,
+    robots, isPending} = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name
         .toLowerCase()
         .includes(searchField.toLowerCase());
     });
 
-    if (!robots.length) {
+    if (isPending) {
       return <h1>Loading...</h1>;
     } else {
       return (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <CardList robots={filteredRobots} />
           </Scroll>
